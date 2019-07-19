8754de6a4b2fc5a77ccf59cca98d2964b81f4d0a
Leif Niemczik <leif.niem@gmail.com>
2019-07-19 13:43:56 +0200

    [sketches/sampletrail.js/sampletrail.js-2019.07.18-19.16.42.js] 2019.07.19-13.43.56

--- sketches/sampletrail.js/sampletrail.js-2019.07.18-19.16.42.js
+++ sketches/sampletrail.js/sampletrail.js-2019.07.18-19.16.42.js
@@ -2,30 +2,27 @@ const canvasSketch = require('canvas-sketch')
 const random = require('canvas-sketch-util/random')
 const color = require('canvas-sketch-util/color')
 const { lerp } = require('canvas-sketch-util/math')
-const fs = require('fs')
-
-const settings = {
-	dimensions: [2048, 2048],
-	name: 'trails',
-	suffix: random.getSeed()
-}
 
 const img = new Image()
 img.crossOrigin = 'Anonymous'
 
+const numX = 10
+const numY = 10
+const points = createGrid(numX, numY)
+const padding = 400
+
 function createGrid(numX, numY) {
 	let points = []
-	const count = numX * numY
-	for (let i = 0; i < count; i++) {
-		const u = (i % numX) / (numX - 1)
-		const v = ~~(i / numY) / (numY - 1)
-		points.push({ u, v })
+	for (let y = 0; y < numY; y++) {
+		for (let x = 0; x < numX; x++) {
+			const u = x / (numX - 1)
+			const v = y / (numY - 1)
+			points.push({ u, v, x, y, visited: false })
+		}
 	}
 	return points
 }
 
-const numX = 10
-const numY = 10
 
 function sampleColors(context, points, width, height, padding) {
 	for (let point of points) {
@@ -41,81 +38,124 @@ function sampleColors(context, points, width, height, padding) {
 	return points
 }
 
-function drawStuff(context, points, width, height, padding) {
-	context.lineWidth = 10
-	context.lineJoin = 'round'
-	context.fillRect(0, 0, width, height)
+function nextPoint(point, dir) {
+	let next = (point.y + dir) * numX + point.x + dir
+	return next
+}
 
-	let point, end, startx, starty, endx, endy
-	while (points.length) {
+function drawStuff(context, points, width, height, padding) {
+	context.lineWidth = 20
+	context.lineJoin = 'round'
+	context.lineCap = 'round'
+
+	color.contrastRatio(random.pick(points).color.rgb, "#ffffff") < 4.5
+	? context.fillStyle = "white"
+	: context.fillStyle = "black"
+	context.fillRect(0, 0, width, height)
+	let linecount = numY
+
+	let point, next, startx, starty, nextx, nexty, dist, dir
+	while (linecount) {
 		point = random.pick(points)
 		context.beginPath()
-		if (random.value() > 0.5) {
-			end = random.pick(points)
+		dist = random.rangeFloor(1, 6)
+		dir = random.sign()
+		next = points[nextPoint(point, dir)]
+		while (
+			next &&
+			dist &&
+			point.x !== 0 &&
+			point.x !== numX - 1 &&
+			!point.visited &&
+			!next.visited
+		) {
 			startx = lerp(padding, width - padding, point.u)
 			starty = lerp(padding, height - padding, point.v)
-			endx = lerp(padding, width - padding, end.u),
-			endy = lerp(padding, height - padding, end.v)
+			nextx = lerp(padding, width - padding, next.u)
+			nexty = lerp(padding, height - padding, next.v)
 			context.moveTo(startx, starty)
-			context.lineTo(
-				endx,
-				endy
-			)
+			context.lineTo(nextx, nexty)
 			const grad = context.createLinearGradient(
 				startx,
 				starty,
-				endx,
-				endy
+				nextx,
+				nexty
 			)
 			grad.addColorStop(0, point.color.hex)
-			grad.addColorStop(1, end.color.hex)
+			grad.addColorStop(1, next.color.hex)
 			context.strokeStyle = grad
 			context.stroke()
-			points.splice(points.indexOf(point), 1)
-			points.splice(points.indexOf(end), 1)
-		} else {
+			// points.splice(points.indexOf(point), 1)
+			point.visited = true
+			point = next
+			next = points[nextPoint(point, dir)]
+			dist--
+		}
+		// points.splice(points.indexOf(point), 1)
+		linecount--
+	}
+
+	while (points.length) {
+		point = random.pick(points)
+		if (!point.visited) {
+			context.beginPath()
 			context.fillStyle = point.color.hex
 			context.arc(
 				lerp(padding, width - padding, point.u),
 				lerp(padding, height - padding, point.v),
-				context.lineWidth,
+				context.lineWidth / 2,
 				0,
 				Math.PI * 2
 			)
 			context.fill()
-			points.splice(points.indexOf(point), 1)
 		}
+		points.splice(points.indexOf(point), 1)
+	}
+}
+
+async function loadImage() {
+	try {
+		let data = await fetch('https://source.unsplash.com/random/2048x2048')
+		// fetch('https://source.unsplash.com/random/2048x2048').then(data => {
+		id = data.url.match(
+			/https:\/\/images\.unsplash\.com\/photo-([\da-f]+-[\da-f]+)/
+		)[1]
+		img.src = data.url
+		return id
+		// })
+	} catch (error) {
+		console.error(error)
 	}
 }
 
 const sketch = () => {
-	const points = createGrid(numX, numY)
-	const padding = 300
-	try {
-		fetch('https://source.unsplash.com/random/2048x2048').then(data => {
-			random.setSeed(
-				data.url.match(
-					/https:\/\/images\.unsplash\.com\/photo-([\da-f]+-[\da-f]+)/
-				)[1]
-			)
-			img.src = data.url
-		})
-	} catch (error) {
-		console.error(error)
-	}
-	return ({ context, width, height }) => {
-		img.onload = () => {
-			context.drawImage(img, 0, 0, width, height)
-			const sampled = sampleColors(
-				context,
-				points,
-				width,
-				height,
-				padding
-			)
-			drawStuff(context, sampled, width, height, padding)
+	return {
+		render({ context, width, height }) {
+			img.onload = () => {
+				context.drawImage(img, 0, 0, width, height)
+				const sampled = sampleColors(
+					context,
+					points,
+					width,
+					height,
+					padding
+				)
+				drawStuff(context, sampled, width, height, padding)
+			}
 		}
 	}
 }
 
-canvasSketch(sketch, settings)
+let settings = {
+	dimensions: [2048, 2048],
+	name: 'trails',
+}
+
+loadImage().then((seed) => {
+	settings.suffix = seed
+	random.setSeed(seed)
+	canvasSketch(sketch, settings)
+})
+
+
+
